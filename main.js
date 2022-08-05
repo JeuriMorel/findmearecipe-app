d2c39b735c223e5be2e81a402147590cb649b332
JeuriMorel <rikonikage@yahoo.com>
2022-08-05 18:10:21 -0400

    feat: basic form design

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+import { qs, qsa } from "./utilities"
 
+const form = qs("form")
+// const button = qs('button')
+const radios = qsa("input[type=radio]")
+
+form.addEventListener("submit", e => {
+    e.preventDefault()
+    let data = new FormData(form)
+    for (const [key, value] of data.entries()) {
+        console.log(key, value)
+    }
+})
+
+radios.forEach(radio => {
+    radio.addEventListener("click", () => {
+        radio.toggleAttribute("data-checked")
+        if (radio.dataset.checked == null) {
+            radio.checked = false
+        }
+    })
+})
