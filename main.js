78ab739c472b837ddcbd96cd99a1cf4f62482599
JeuriMorel <rikonikage@yahoo.com>
2022-08-09 20:02:47 -0400

    feat: basic recipe card layout

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 import { qs, qsa } from "./utilities"
-import {response} from './example'
+import { response } from "./example"
 
 const form = qs("form")
 const keywordsContainer = qs("[data-keywords]")
 // const button = qs('button')
 const radios = qsa("input[type=radio]")
+const resultsContainer = qs("[data-container=results]")
 
 form.addEventListener("submit", e => {
     e.preventDefault()
@@ -17,7 +18,11 @@ form.addEventListener("submit", e => {
     // console.log("diet: ", diet)
     // console.log("allergies: ", allergies)
     // console.log("cuisine: ", cuisine)
+    resultsContainer.innerHTML = ""
     getRecipes()
+    resultsContainer.scrollIntoView({
+        behavior: "smooth",
+    })
 })
 
 form.addEventListener("change", e => {
@@ -42,17 +47,17 @@ function handleRadioKeywordBtns() {
 
 function removeKeywordBtn(val) {
     let keywordBtn = qs(`[data-keyword=${val}]`)
-    if(keywordBtn) keywordBtn.remove()
+    if (keywordBtn) keywordBtn.remove()
 }
 
 function createKeywordBtn(val, isRadio) {
     let el = document.createElement("button")
     el.type = "button"
     el.textContent = val
-    el.classList.add('keyword-btn')
+    el.classList.add("keyword-btn")
     el.setAttribute("data-radio", isRadio)
     el.setAttribute("data-keyword", val)
-    el.addEventListener('click', () => {
+    el.addEventListener("click", () => {
         let input = qs(`#${val}`)
         input.removeAttribute("data-checked")
         input.checked = false
@@ -77,17 +82,95 @@ radios.forEach(radio => {
 
 //cuisineType=Eastern%20Europe
 
-    //https://api.edamam.com/api/recipes/v2?type=public&q=shrimp%20scampi%20alfredo&app_id=45433615&app_key=cec90a00&diet=balanced&diet=high-fiber&diet=high-protein&diet=low-carb&health=alcohol-cocktail&health=alcohol-free&cuisineType=Asian
-
-
+//https://api.edamam.com/api/recipes/v2?type=public&q=shrimp%20scampi%20alfredo&app_id=45433615&app_key=cec90a00&diet=balanced&diet=high-fiber&diet=high-protein&diet=low-carb&health=alcohol-cocktail&health=alcohol-free&cuisineType=Asian
 
 function getRecipes() {
     let recipes = response.hits
-    recipes.forEach(handleRecipe)
+    recipes.forEach(handleRecipeCards)
 }
 
-function handleRecipe(recipe) {
-    const { label, images, source, url, yield: servings, ingredients, calories, totalTime, totalNutrients, totalDaily, digest } = recipe.recipe
-    
-    console.dir({ label, images, source, url, servings, ingredients, calories, totalTime, totalNutrients, totalDaily, digest })
+function handleRecipeCards(recipe) {
+    const {
+        label,
+        images,
+        source,
+        url,
+        yield: servings,
+        ingredients,
+        calories,
+        totalTime,
+        totalNutrients,
+        totalDaily,
+        digest,
+        healthLabels,
+        cautions,
+    } = recipe.recipe
+
+    let card = document.createElement("div")
+    card.classList.add("card")
+    card.innerHTML = `
+                    <img src="https://source.unsplash.com/300x300/?food" alt="" class="card-thumbnail">
+                    <p class="recipe-name">${label}</p>
+                    <p class="[ recipe-info ] subtitle"><span>${Math.round(
+                        calories
+                    )}kcal</span><span>servings: ${servings}</span></p>
+                    <a href=${url} target="_blank"
+                    rel="noreferrer nofollow" class="link">go to: ${source}</a>
+                    <p class="[ caution ] subtitle">Contains: ${cautions.join(
+                        "&#8226;"
+                    )}</p>
+                    <details>
+                    <summary>calorie breakdown</summary>
+                    <div class="canvas-container">
+                    <canvas></canvas>
+                    <span class="[ calorie-display ]">${Math.round(
+                        calories
+                    )}</span>
+                    </div>
+                    </details>
+                    
+                `
+
+    resultsContainer.append(card)
+    addChartToCanvas(totalNutrients, card)
+    // console.log(images.THUMBNAIL)
+}
+
+function addChartToCanvas({ FAT: fat, CHOCDF: carbs, PROCNT: protein }, card) {
+    let macrosArray = [fat, carbs, protein].map(macro => new Macro(macro))
+
+    let donutCanvas = qs("canvas", card).getContext("2d")
+
+    let donutChart = new Chart(donutCanvas, {
+        type: "doughnut",
+        data: {
+            labels: macrosArray.map(macro => macro.label),
+            datasets: [
+                {
+                    label: "calories",
+                    data: macrosArray,
+                    backgroundColor: ["#7d5204", "#e19306", "#fdd183"],
+                    cutout: "85%",
+                    borderColor: "#fff6e6",
+                    borderWidth: 4,
+                    hoverBorderWidth: 1,
+                    hoverBorderJoinStyle: "round",
+                    borderAlign: "center",
+                },
+            ],
+        },
+    })
+}
+
+class Macro {
+    constructor({ label, quantity }) {
+        this.label = label
+        this.quantity = Math.round(quantity)
+        this.multiplier = label == "Fat" ? 9 : 4
+        this.value = this.calories
+    }
+
+    get calories() {
+        return this.multiplier * this.quantity
+    }
 }
